98caaa803d0a6cdb99c7e9a55aef8fed13025ccd
Felix Sun <sunfmin@gmail.com>
2017-10-28 13:29:19 +0800

    fix didn't use prefix

--- utils.go
+++ utils.go
@@ -4,24 +4,37 @@ import (
 	"github.com/qiniu/api.v7/storage"
 )
 
-type udata struct {
+type Qn struct {
 	bm     *storage.BucketManager
 	bucket string
 	prefix string
 }
 
-func Bucket(bm *storage.BucketManager, bucket string) (r *udata) {
-	r = &udata{bm, bucket, ""}
+func clone(u *Qn) (r *Qn) {
+	r = &Qn{}
+	r.bm = u.bm
+	r.bucket = u.bucket
+	r.prefix = u.prefix
 	return
 }
 
-func (u *udata) Prefix(prefix string) (r *udata) {
+func New() *Qn {
+	return &Qn{}
+}
+
+func (q *Qn) Bucket(bm *storage.BucketManager, bucket string) (r *Qn) {
+	r = clone(q)
+	r = &Qn{bm, bucket, ""}
+	return
+}
+
+func (u *Qn) Prefix(prefix string) (r *Qn) {
 	u.prefix = prefix
 	r = u
 	return
 }
 
-func (u *udata) ForEach(limit int, f func(entries []storage.ListItem, commonPrefixes []string) error) (err error) {
+func (u *Qn) ForEach(limit int, f func(entries []storage.ListItem, commonPrefixes []string) error) (err error) {
 	var (
 		entries        []storage.ListItem
 		commonPrefixes []string
@@ -29,8 +42,11 @@ func (u *udata) ForEach(limit int, f func(entries []storage.ListItem, commonPref
 		hasNext        bool = true
 	)
 	for hasNext {
-		entries, commonPrefixes, nextMarker, hasNext, err = u.bm.ListFiles(u.bucket, "", "", nextMarker, 1000)
+		entries, commonPrefixes, nextMarker, hasNext, err = u.bm.ListFiles(u.bucket, u.prefix, "", nextMarker, 1000)
 		if err != nil {
+			return
+		}
+		if len(entries) == 0 {
 			return
 		}
 		err = f(entries, commonPrefixes)
