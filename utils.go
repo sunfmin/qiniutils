fdce680755b0e729f364a64349db6109a97efe8f
Felix Sun <sunfmin@gmail.com>
2017-10-28 14:10:44 +0800

    Can make url now

--- utils.go
+++ utils.go
@@ -1,40 +1,127 @@
 package qiniutils
 
 import (
+	"time"
+
+	"github.com/qiniu/api.v7/auth/qbox"
 	"github.com/qiniu/api.v7/storage"
 )
 
-type Qn struct {
-	bm     *storage.BucketManager
-	bucket string
-	prefix string
+type Qiniu struct {
+	bm            *storage.BucketManager
+	bucket        string
+	prefix        string
+	storageConfig *storage.Config
+	mac           *qbox.Mac
 }
 
-func clone(u *Qn) (r *Qn) {
-	r = &Qn{}
+type URLMaker struct {
+	qn     *Qiniu
+	domain string
+	key    string
+	ttl    time.Duration
+}
+
+func (u *Qiniu) clone() (r *Qiniu) {
+	r = &Qiniu{}
 	r.bm = u.bm
 	r.bucket = u.bucket
 	r.prefix = u.prefix
+	r.mac = u.mac
+	r.storageConfig = u.storageConfig
 	return
 }
 
-func New() *Qn {
-	return &Qn{}
+func New() *Qiniu {
+	return &Qiniu{}
 }
 
-func (q *Qn) Bucket(bm *storage.BucketManager, bucket string) (r *Qn) {
-	r = clone(q)
-	r = &Qn{bm, bucket, ""}
+func (u *URLMaker) clone() (r *URLMaker) {
+	r = &URLMaker{}
+	r.qn = u.qn
+	r.domain = u.domain
+	r.key = u.key
+	r.ttl = u.ttl
 	return
 }
 
-func (u *Qn) Prefix(prefix string) (r *Qn) {
-	u.prefix = prefix
-	r = u
+func (q *Qiniu) URL() (r *URLMaker) {
+	r = &URLMaker{}
+	r.qn = q
 	return
 }
 
-func (u *Qn) ForEach(limit int, f func(entries []storage.ListItem, commonPrefixes []string) error) (err error) {
+func (u *URLMaker) Domain(domain string) (r *URLMaker) {
+	r = u.clone()
+	r.domain = domain
+	return
+}
+
+func (u *URLMaker) TTL(ttl time.Duration) (r *URLMaker) {
+	r = u.clone()
+	r.ttl = ttl
+	return
+}
+
+func (u *URLMaker) Key(key string) (r *URLMaker) {
+	r = u.clone()
+	r.key = key
+	return
+}
+
+func (u *URLMaker) PublicURL() (r string) {
+	storage.MakePublicURL(u.domain, u.key)
+	return
+}
+
+func (u *URLMaker) PrivateURL() (r string) {
+	deadline := time.Now().Add(u.ttl).Unix()
+	return storage.MakePrivateURL(u.qn.mac, u.domain, u.key, deadline)
+}
+
+func (q *Qiniu) Mac(accessKey, secretKey string) (r *Qiniu) {
+	r = q.clone()
+	r.mac = qbox.NewMac(accessKey, secretKey)
+	return
+}
+
+func (q *Qiniu) StorageConfig(cfg *storage.Config) (r *Qiniu) {
+	r = q.clone()
+	r.storageConfig = cfg
+	return
+}
+
+func (q *Qiniu) Bucket(bucket string) (r *Qiniu) {
+	r = q.clone()
+	r.bucket = bucket
+	return
+}
+
+func (q *Qiniu) Prefix(prefix string) (r *Qiniu) {
+	r = q.clone()
+	r.prefix = prefix
+	return
+}
+
+func (u *Qiniu) bucketManager() *storage.BucketManager {
+	if u.bm != nil {
+		return u.bm
+	}
+
+	if u.mac == nil {
+		panic("call .Mac(xx) first to set mac")
+	}
+
+	if u.storageConfig == nil {
+		panic("call .StorageConfig(xx) to set storage config")
+	}
+
+	u.bm = storage.NewBucketManager(u.mac, u.storageConfig)
+
+	return u.bm
+}
+
+func (u *Qiniu) ForEach(limit int, f func(entries []storage.ListItem, commonPrefixes []string) error) (err error) {
 	var (
 		entries        []storage.ListItem
 		commonPrefixes []string
@@ -42,7 +129,7 @@ func (u *Qn) ForEach(limit int, f func(entries []storage.ListItem, commonPrefixe
 		hasNext        bool = true
 	)
 	for hasNext {
-		entries, commonPrefixes, nextMarker, hasNext, err = u.bm.ListFiles(u.bucket, u.prefix, "", nextMarker, 1000)
+		entries, commonPrefixes, nextMarker, hasNext, err = u.bucketManager().ListFiles(u.bucket, u.prefix, "", nextMarker, 1000)
 		if err != nil {
 			return
 		}
@@ -54,5 +141,10 @@ func (u *Qn) ForEach(limit int, f func(entries []storage.ListItem, commonPrefixe
 			return
 		}
 	}
+	return
+}
+
+func (q *Qiniu) GetBuckets(shared bool) (buckets []string, err error) {
+	buckets, err = q.bucketManager().Buckets(true)
 	return
 }
--- utils_test.go
+++ utils_test.go
@@ -4,21 +4,23 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
-	"github.com/qiniu/api.v7/auth/qbox"
 	"github.com/qiniu/api.v7/storage"
 	"github.com/sunfmin/qiniutils"
 )
 
 func TestForEach(t *testing.T) {
 	var err error
-	mac := qbox.NewMac(os.Getenv("QINIU_AccessID"), os.Getenv("QINIU_AccessKey"))
+
+	qn := qiniutils.New()
 	storageCfg := &storage.Config{}
 	storageCfg.Zone = &storage.ZoneHuadong
-	bm := storage.NewBucketManager(mac, storageCfg)
+	qn = qn.Mac(os.Getenv("QINIU_AccessID"), os.Getenv("QINIU_AccessKey")).
+		StorageConfig(storageCfg)
 
 	var buckets []string
-	buckets, err = bm.Buckets(true)
+	buckets, err = qn.GetBuckets(true)
 	if err != nil {
 		panic(err)
 	}
@@ -26,10 +28,10 @@ func TestForEach(t *testing.T) {
 	i := 0
 	for _, bucket := range buckets {
 		fmt.Printf("bucket: %s\n", bucket)
-		err = qiniutils.Bucket(bm, bucket).ForEach(1000, func(entries []storage.ListItem, commonPrefixes []string) error {
+		err = qn.Bucket(bucket).ForEach(1000, func(entries []storage.ListItem, commonPrefixes []string) error {
 			for _, en := range entries {
 				i++
-				fmt.Println(i, en.Key)
+				fmt.Println(i, en.Key, qn.URL().Domain("sunfmin.com").TTL(time.Hour).Key(en.Key).PrivateURL())
 			}
 			return nil
 		})
